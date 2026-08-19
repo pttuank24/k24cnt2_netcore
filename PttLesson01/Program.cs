3f2bd5998ee3ebc275b11cdea19a6db0a24a7f8c
pttuank24 <phamtientuan2006@gmail.com>
2026-08-19 17:21:19 +0700

    PttuanLesson01 - First WebApp

---
+++ PttLesson01/Program.cs
@@ -0,0 +1,15 @@
+namespace PttLesson01
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var app = builder.Build();
+
+            app.MapGet("/", () => "Hello!pham tuan ");
+
+            app.Run();
+        }
+    }
+}
