637c2b64644e3b8399dc137ac3b60207149005cf
pttuank24 <phamtientuan2006@gmail.com>
2026-08-21 22:20:40 +0700

    Them project MyAppMVC

---
+++ MyAppMVC/Controllers/ProductController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyAppMVC.Models;
+
+namespace MyAppMVC.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            var products = new List<Product>
+            {
+                new Product { Id = 1, Name = "Product 1", Price = 500000, CreatedAt = new DateTime(2020,12,25), Image = "product1.webp" },
+                new Product { Id = 2, Name = "Product 2", Price = 700000, CreatedAt = new DateTime(2020,12,25), Image = "product2.webp" },
+                new Product { Id = 3, Name = "Product 3", Price = 550000, CreatedAt = new DateTime(2020,12,25), Image = "product3.webp" },
+                new Product { Id = 4, Name = "Product 4", Price = 550000, CreatedAt = new DateTime(2020,12,25), Image = "product4.webp" },
+            };
+
+            return View(products);
+        }
+    }
+}
