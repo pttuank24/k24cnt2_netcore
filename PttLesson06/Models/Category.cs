788564c4ed03895e50ecb15a748e903cb4d5b9d0
pttuank24 <phamtientuan2006@gmail.com>
2026-09-09 17:51:36 +0700

    PttLesson06

---
+++ PttLesson06/Models/Category.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PttLesson06.Models
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
---
+++ PttLesson06/ViewComponents/CategoryViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PttLesson06.Models;
+
+namespace PttLesson06.ViewComponent
+{
+    public class CategoryViewComponent : Microsoft.AspNetCore.Mvc.ViewComponent
+    {
+        public IViewComponentResult Invoke(int? n)
+        {
+            List<Category> categories = new List<Category>
+            {
+                new Category {CategoryId = 1, CategoryName = "Electronics", IsActive = true},
+                new Category {CategoryId = 2, CategoryName = "Books", IsActive = true },
+                new Category {CategoryId = 3, CategoryName = "Clothing", IsActive = false },
+                new Category {CategoryId = 4, CategoryName = "Home & Kitchen", IsActive = true}
+            };
+
+            n = n ?? 0;
+            var search = categories.Where(c => c.CategoryId> n).ToList();
+            return View(categories);
+            
+        }
+    }
+}
